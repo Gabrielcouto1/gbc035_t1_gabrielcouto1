7c5dc73840ec23d6fc57b65b9c8a809b81ca56a7
Gabriel Couto de Freitas <gabriel.freitas.pb@compasso.com.br>
2023-05-02 09:20:06 -0300

    feat: lab3 pronto

---
+++ LAB03 - Class Relations/Ex01/src/Aluno.java
@@ -0,0 +1,11 @@
+public class Aluno {
+    private int CPF;
+    private String nome, matricula;
+    private 
+
+    public Aluno(int CPF, String nome, String matricula) {
+        this.CPF=CPF;
+        this.nome=nome;
+        this.matricula=matricula;
+    }
+}
